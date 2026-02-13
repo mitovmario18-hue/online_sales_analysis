0616530cb1ac7bfaf20289f9e7ca8d5c212c4837
Mario Mitov <mitovmario18@gmail.com>
2026-02-13 20:47:51 +0100

    Implemented remove_product method

--- product_manager.py
+++ product_manager.py
@@ -5,10 +5,15 @@ class ProductManager:
     def __init__(self):
         self.products = []
 
-    def add_product(self, product: Product):
+    def add_product(self, product):
         self.products.append(product)
+        print(f"Product '{product.name}' added.")
 
     def display_products(self):
+        if not self.products:
+            print("No products available.")
+            return
+
         for product in self.products:
             product.display_info()
 
@@ -17,10 +22,11 @@ class ProductManager:
         for product in self.products:
             total += product.price * product.quantity
         return total
+
     def remove_product(self, name):
-    for product in self.products:
-        if product.name == name:
-            self.products.remove(product)
-            print(f"Product '{name}' removed successfully.")
-            return
-    print(f"Product '{name}' not found.")
+        for product in self.products:
+            if product.name == name:
+                self.products.remove(product)
+                print(f"Product '{name}' removed successfully.")
+                return
+        print(f"Product '{name}' not found.")
