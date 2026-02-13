8614357e970f0cf27f29469865324d6dd47d344f
Mario Mitov <mitovmario18@gmail.com>
2026-02-13 20:53:07 +0100

    Added Cart class and cart functionality in main

--- main.py
+++ main.py
@@ -1,22 +1,35 @@
 from product import Product
 from product_manager import ProductManager
+from cart import Cart
 
 
 def main():
     manager = ProductManager()
 
-    p1 = Product("Laptop", 1200, 5)
-    p2 = Product("Mouse", 25, 20)
-    p3 = Product("Keyboard", 75, 10)
+    # dostupni proizvodi
+    p1 = Product("Laptop", 1200, 2)
+    p2 = Product("Mouse", 25, 3)
+    p3 = Product("Keyboard", 75, 1)
+    p4 = Product("Monitor", 300, 1)
 
     manager.add_product(p1)
     manager.add_product(p2)
     manager.add_product(p3)
+    manager.add_product(p4)
 
-    print("Available products:")
-    manager.display_products()
+    # korpa
+    cart = Cart()
 
-    print("\nTotal inventory value:", manager.total_inventory_value())
+    # dodaj 3 proizvoda iz liste dostupnih proizvoda (iz managera)
+    cart.add_to_cart(manager.products[0])
+    cart.add_to_cart(manager.products[1])
+    cart.add_to_cart(manager.products[2])
+
+    print("\nCart content:")
+    cart.display_cart()
+
+    print("\nTotal to pay:")
+    print(cart.calculate_total())
 
 
 if __name__ == "__main__":
